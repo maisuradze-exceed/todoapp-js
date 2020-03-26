bcced5ec8df048fd3625da47753b4ba1d9f8bd05
maisuradze-exceed <g.maisuradze.exceedteam@gmail.com>
2020-03-26 17:43:16 +0400

    finished for testing

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const addButton = document.querySelector('.addtodo-btn');
 const todoValue = document.querySelector('.todo-value');
 const todoList = document.querySelector('.todo-list');
-var removeTodo = document.querySelectorAll('img');
 const completedAll = document.querySelector('.allcomplete-btn');
+let remove = document.querySelectorAll('img');
+let checkComplete = document.querySelectorAll('.check')
 
 // Add New Todo
 addButton.addEventListener('click', () => {
@@ -16,6 +17,7 @@ addButton.addEventListener('click', () => {
         //creating checkbox
         let checkbox = document.createElement('input')
         checkbox.type = 'checkbox'
+        checkbox.className = 'check'
         // creating image
         let image = document.createElement('img')
         image.src = "./img/remove.png"
@@ -31,12 +33,54 @@ addButton.addEventListener('click', () => {
 })
 
 
-//Remove Todo
+
 const updateTodo = () => {
-    removeTodo = document.querySelectorAll('img');
-    removeTodo.forEach(todo => {
-        todo.addEventListener('click', (e) => {
-            e.target.parentNode.remove()
-        })
-    })
+
+    //Complete Todo
+    removeTodo();
+    completeTodo();
+    check();
 }
+
+//Remove Todo
+const removeTodo = () => {
+    remove = document.querySelectorAll('img');
+    remove.forEach(todo => {
+        todo.addEventListener('click', (event) => {
+            event.target.parentNode.remove()
+        })
+    });
+}
+
+// Complete Todo
+const completeTodo = () => {
+    checkComplete = document.querySelectorAll('.check')
+    checkComplete.forEach(check => {
+        check.addEventListener('change', (event) => {
+            if (event.target.checked) {
+                event.target.parentNode.children[1].className = 'done'
+            } else {
+                event.target.parentNode.children[1].className = ''
+            }
+        })
+    });
+}
+
+
+//Complete All
+const check = () => 
+completedAll.addEventListener('click', () => {
+    checkComplete.forEach(check => {
+        check.parentNode.children[0].checked = true;
+        check.parentNode.children[1].className = 'done';
+    })
+})
+
+
+
+
+
+
+
+//Initializing calls
+check();
