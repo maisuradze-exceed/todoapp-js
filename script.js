01713521f3afc5301231e7d3f663776335e0bd2a
maisuradze-exceed <g.maisuradze.exceedteam@gmail.com>
2020-03-26 17:05:58 +0400

    added logic for new todo

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello app')
+const addButton = document.querySelector('.addtodo-btn');
+const todoValue = document.querySelector('.todo-value');
+const todoList = document.querySelector('.todo-list')
+
+
+// Add New Todo
+addButton.addEventListener('click', () => {
+    if (todoValue.value.length !== 0) {
+        let value = todoValue.value;
+        let div = document.createElement('div');
+        div.className = 'todo-item';
+        //creating p element
+        let p = document.createElement('p')
+        p.innerHTML = value;
+        //creating checkbox
+        let checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        // creating image
+        let image = document.createElement('img')
+        image.src = "./img/remove.png"
+        //appending into div
+        div.append(checkbox)
+        div.append(p)
+        div.append(image)
+        //adding to main
+        todoList.append(div)
+        todoValue.value = ""
+    } 
+})
