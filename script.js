84824ecb407d2617040bd51f2d6edf9221eae00f
maisuradze-exceed <g.maisuradze.exceedteam@gmail.com>
2020-03-27 05:16:42 +0400

    finished task

--- script.js
+++ script.js
@@ -2,49 +2,86 @@ const addButton = document.querySelector('.addtodo-btn');
 const todoValue = document.querySelector('.todo-value');
 const todoList = document.querySelector('.todo-list');
 const completedAll = document.querySelector('.allcomplete-btn');
-let remove = document.querySelectorAll('img');
+let remove = document.querySelectorAll('.remove');
+let edit = document.querySelectorAll('.edit');
 let checkComplete = document.querySelectorAll('.check');
+let save = document.querySelectorAll('.edit-text');
+let text = document.querySelectorAll('.text');
 
 // Add New Todo
 addButton.addEventListener('click', () => {
   if (todoValue.value.length) {
-    let value = todoValue.value;
-    let div = document.createElement('div');
-    div.className = 'todo-item';
-    //creating p element
-    let p = document.createElement('p');
-    p.innerHTML = value;
-    //creating checkbox
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'check';
-    // creating image
-    let image = document.createElement('img');
-    image.src = './img/remove.png';
-    //appending into div
-    div.append(checkbox);
-    div.append(p);
-    div.append(image);
-    //adding to main
-    todoList.append(div);
-    todoValue.value = '';
-    updateTodo();
+    if (todoValue.value.replace(/\s/g, '').length) {
+      template();
+      updateTodo();
+    } else {
+      todoValue.value = '';
+    }
+  }
+});
+
+todoValue.addEventListener('keyup', event => {
+  if (event.keyCode === 13) {
+    if (todoValue.value.length) {
+      if (todoValue.value.replace(/\s/g, '').length) {
+        template();
+        updateTodo();
+      } else {
+        todoValue.value = '';
+      }
+    }
   }
 });
 
 const updateTodo = () => {
-  //Complete Todo
   removeTodo();
   completeTodo();
+  editTodo();
 };
 
 //Remove Todo
 const removeTodo = () => {
-  remove = document.querySelectorAll('img');
+  remove = document.querySelectorAll('.remove');
   remove.forEach(todo => {
     todo.addEventListener('click', event => {
-      event.target.parentNode.remove();
+      event.target.parentNode.parentNode.remove();
     });
+  });
+};
+
+//Edit Todo
+const editTodo = () => {
+  edit = document.querySelectorAll('.edit');
+  edit.forEach(todo => {
+    todo.addEventListener('click', editLogic);
+  });
+};
+
+const editLogic = () => {
+  let data = event.target.parentNode.parentNode.children[1].innerHTML;
+  let save = document.createElement('button');
+  save.className = 'save-button';
+  save.innerHTML = 'Save';
+  let input = document.createElement('input');
+  input.type = 'text';
+  input.className = 'edit-text';
+  input.maxLength = 45;
+  input.value = data;
+  event.target.parentNode.className = 'hidden';
+  event.target.parentNode.parentNode.children[0].className = 'hidden';
+  event.target.parentNode.parentNode.children[1].className = 'hidden';
+  event.target.parentNode.parentNode.append(input);
+  event.target.parentNode.parentNode.append(save);
+  save.addEventListener('click', event => {
+    let data = event.target.parentNode.children[3].value;
+    event.target.parentNode.children[0].className = 'check';
+    event.target.parentNode.children[1].className = 'text';
+    if (data) {
+      event.target.parentNode.children[1].innerHTML = data;
+    }
+    event.target.parentNode.children[2].className = 'images';
+    event.target.parentNode.children[3].remove();
+    event.target.parentNode.children[3].remove();
   });
 };
 
@@ -56,41 +93,81 @@ const completeTodo = () => {
       if (event.target.checked) {
         event.target.parentNode.children[1].className = 'done';
       } else {
-        event.target.parentNode.children[1].className = '';
+        event.target.parentNode.children[1].className = 'text';
       }
     });
   });
 };
 
 //Check if complete
-const check = () => {
+const checkTodo = () => {
+  save = document.querySelectorAll('.edit-text');
   let unchecked = true;
   completedAll.addEventListener('click', () => {
     if (unchecked) {
-      completedAll.innerHTML = 'Uncomplete All';
       unchecked = false;
       selectAll();
     } else {
-      completedAll.innerHTML = 'Complete All';
       unchecked = true;
       UnSelectAll();
     }
   });
   const selectAll = () => {
     checkComplete.forEach(check => {
-      check.checked = true;
-      check.parentNode.children[1].className = 'done';
+      save = document.querySelectorAll('.edit-text');
+      if (save.length == 0) {
+        check.checked = true;
+        check.parentNode.children[1].className = 'done';
+        completedAll.innerHTML = 'Uncomplete All';
+      }
     });
   };
   const UnSelectAll = () => {
     checkComplete.forEach(check => {
-      check.checked = false;
-      check.parentNode.children[1].className = '';
+      if (save.length == 0) {
+        check.checked = false;
+        check.parentNode.children[1].className = '';
+        completedAll.innerHTML = 'Complete All';
+      }
     });
   };
 };
 
+//template
+const template = () => {
+  let value = todoValue.value;
+  let div = document.createElement('div');
+  div.className = 'todo-item';
+  //creating p element
+  let p = document.createElement('p');
+  p.innerHTML = value;
+  p.className = 'text';
+  //creating checkbox
+  let checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'check';
+  // creating image
+  let imagediv = document.createElement('div');
+  let remove = document.createElement('img');
+  let edit = document.createElement('img');
+  imagediv.className = 'images';
+  remove.src = './img/remove.png';
+  remove.className = 'remove';
+  edit.src = './img/edit-icon.png';
+  edit.className = 'edit';
+  imagediv.append(edit);
+  imagediv.append(remove);
+  //appending into div
+  div.append(checkbox);
+  div.append(p);
+  div.append(imagediv);
+  //adding to main
+  todoList.append(div);
+  todoValue.value = '';
+};
+
 //Initializing calls
 removeTodo();
-check();
+checkTodo();
 completeTodo();
+editTodo();
