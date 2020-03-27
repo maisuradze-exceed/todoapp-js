30569da38077b725e64a1d9c30ddb87d98b5436b
maisuradze-exceed <g.maisuradze.exceedteam@gmail.com>
2020-03-27 14:26:41 +0400

    adding paginator

--- script.js
+++ script.js
@@ -7,46 +7,96 @@ let edit = document.querySelectorAll('.edit');
 let checkComplete = document.querySelectorAll('.check');
 let save = document.querySelectorAll('.edit-text');
 let text = document.querySelectorAll('.text');
+let unchecked = true;
+let pagination = document.querySelectorAll('.todo-item');
+let myArr = [];
+let paginationNum = document.querySelector('.pagination');
+let current_page = 1;
+let rows = 10;
+
+// Pagination
+pagination.forEach(pag => myArr.push(pag));
+const displayList = (items, wrapper, rows_per_page, page) => {
+  wrapper.innerHTML = '';
+  page--;
+
+  let start = rows_per_page * page;
+  let end = start + rows_per_page;
+  let pagianetItems = items.slice(start, end);
+  for (let i = 0; i < pagianetItems.length; i++) {
+    let item = pagianetItems[i];
+    wrapper.append(item);
+  }
+};
+
+const setupPagination = (items, wrapper, rows_per_page) => {
+  wrapper.innerHTML = '';
+  console.log(wrapper);
+  let page_count = Math.ceil(items.length / rows_per_page);
+  for (let i = 1; i < page_count + 1; i++) {
+    let btn = paginationButton(i, items);
+    wrapper.appendChild(btn);
+  }
+};
+
+const paginationButton = (page, items) => {
+  let button = document.createElement('button');
+  button.innerText = page;
+  if (current_page === page) {
+    button.classList.add('active');
+  }
+
+  button.addEventListener('click', () => {
+    current_page = page;
+    displayList(items, todoList, rows, current_page);
+
+    let current_btn = document.querySelector('.active');
+    current_btn.classList.remove('active');
+
+    button.classList.add('active');
+  });
+
+  return button;
+};
+
+displayList(myArr, todoList, rows, current_page);
+setupPagination(myArr, paginationNum, rows);
 
 // Add New Todo
 addButton.addEventListener('click', () => {
-  if (todoValue.value.length) {
-    if (todoValue.value.replace(/\s/g, '').length) {
+  if (todoValue.value.length && todoValue.value.trim().length) {
+    template();
+  } else {
+    todoValue.value = '';
+  }
+});
+
+todoValue.addEventListener('keyup', event => {
+  if (event.keyCode === 13 && todoValue.value.length) {
+    if (todoValue.value.trim().length) {
       template();
-      updateTodo();
     } else {
       todoValue.value = '';
     }
   }
 });
 
-todoValue.addEventListener('keyup', event => {
-  if (event.keyCode === 13) {
-    if (todoValue.value.length) {
-      if (todoValue.value.replace(/\s/g, '').length) {
-        template();
-        updateTodo();
-      } else {
-        todoValue.value = '';
-      }
-    }
-  }
-});
-
-const updateTodo = () => {
-  removeTodo();
-  completeTodo();
-  editTodo();
-};
+const updateTodo = () => {};
 
 //Remove Todo
 const removeTodo = () => {
-  remove = document.querySelectorAll('.remove');
-  remove.forEach(todo => {
-    todo.addEventListener('click', event => {
-      event.target.parentNode.parentNode.remove();
+  let myArr1 = myArr;
+  myArr1.forEach((todo, index) => {
+    todo.children[2].children[1].addEventListener('click', () => {
+      myArr1.splice(index, 1);
+      updateTodoArray(myArr1);
     });
   });
+};
+
+const updateTodoArray = value => {
+  displayList(value, todoList, rows, current_page);
+  setupPagination(value, paginationNum, rows);
 };
 
 //Edit Todo
@@ -76,7 +126,7 @@ const editLogic = () => {
     let data = event.target.parentNode.children[3].value;
     event.target.parentNode.children[0].className = 'check';
     event.target.parentNode.children[1].className = 'text';
-    if (data) {
+    if (data.trim()) {
       event.target.parentNode.children[1].innerHTML = data;
     }
     event.target.parentNode.children[2].className = 'images';
@@ -102,29 +152,28 @@ const completeTodo = () => {
 //Check if complete
 const checkTodo = () => {
   save = document.querySelectorAll('.edit-text');
-  let unchecked = true;
   completedAll.addEventListener('click', () => {
     if (unchecked) {
       unchecked = false;
       selectAll();
     } else {
       unchecked = true;
-      UnSelectAll();
+      unSelectAll();
     }
   });
   const selectAll = () => {
     checkComplete.forEach(check => {
       save = document.querySelectorAll('.edit-text');
-      if (save.length == 0) {
+      if (!save.length) {
         check.checked = true;
         check.parentNode.children[1].className = 'done';
         completedAll.innerHTML = 'Uncomplete All';
       }
     });
   };
-  const UnSelectAll = () => {
+  const unSelectAll = () => {
     checkComplete.forEach(check => {
-      if (save.length == 0) {
+      if (!save.length) {
         check.checked = false;
         check.parentNode.children[1].className = '';
         completedAll.innerHTML = 'Complete All';
@@ -162,7 +211,9 @@ const template = () => {
   div.append(p);
   div.append(imagediv);
   //adding to main
-  todoList.append(div);
+  myArr.push(div);
+  displayList(myArr, todoList, rows, current_page);
+  setupPagination(myArr, paginationNum, rows);
   todoValue.value = '';
 };
 
