403dd8465bf6306d9da4a185add5ca1dd96b4d8d
maisuradze-exceed <g.maisuradze.exceedteam@gmail.com>
2020-04-01 18:16:30 +0400

    added server side pagination.

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -12,15 +12,47 @@ router.get('/', async (req, res) => {
   }
 });
 
-//Get Single item
-router.get('/:id', async (req, res) => {
-  try {
-    const item = await Item.findById(req.params.id);
-    res.json.send(res);
-  } catch (err) {
-    res.send({ msg: err });
-  }
+//Paginated Items
+router.get('/lists', paginatedResults(Item), (req, res) => {
+  res.json(res.paginatedResults);
 });
+
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      };
+    }
+    try {
+      results.results = await model
+        .find()
+        .limit(limit)
+        .skip(startIndex)
+        .exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
+}
 
 //Create item
 router.post('/', async (req, res) => {
